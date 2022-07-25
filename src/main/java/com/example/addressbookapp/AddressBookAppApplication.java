ddbad5407cfc8c334c95ebfa71cf388c062244b4
Dhanshri911 <dszingade911@gmail.com>
2022-07-26 00:38:27 +0530

    Added:Section3_UC2 Lombok Library for Logging

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -1,14 +1,17 @@
 package com.example.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookAppApplication.class, args);
-        System.out.println("Welcome to Address Book App");
+        log.info("Address Book Application Started!!!");
+        System.out.println("Welcome to Address Book Program");
     }
 
 }
