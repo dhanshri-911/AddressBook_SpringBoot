07287333c5ca940ff3eb4baf6eef91219eb96e6e
Dhanshri911 <dszingade911@gmail.com>
2022-07-26 22:01:34 +0530

    Added:Section4_UC3 dataConnection and Sorting

--- src/main/java/com/example/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/example/addressbookapp/model/AddressBookData.java
@@ -4,34 +4,52 @@ package com.example.addressbookapp.model;
 import com.example.addressbookapp.dto.AddressBookDTO;
 import lombok.Data;
 
-    @Data
-    //Added Lombok Library for setter getter
+import javax.persistence.*;
+
+@Data                           // Lombak library for getters and setters...
+ @Entity
+ @Table(name = "address_book")
+
+//Added Lombok Library for setter getter
 
     public class AddressBookData {
-        private int personId;
-        private String firstName;
-        private String lastName;
-        private String phoneNumber;
-        private String email;
-        private String address;
-        private String city;
-        private String state;
-        private int zip;
+     @Id
+     @GeneratedValue
+     @Column(name = "id")
+     private int personId;
+     private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
 
 
-        public AddressBookData() {
-        }
-
-
-          public AddressBookData(int personId, AddressBookDTO addressBookDTO) {
-            this.personId = personId;
-            this.firstName = addressBookDTO.firstName;
-            this.lastName = addressBookDTO.lastName;
-            this.phoneNumber = addressBookDTO.phoneNumber;
-            this.email = addressBookDTO.email;
-            this.address = addressBookDTO.address;
-            this.city = addressBookDTO.city;
-            this.state = addressBookDTO.state;
-            this.zip = addressBookDTO.zip;
-        }
+    public AddressBookData() {
     }
+
+
+    public AddressBookData(AddressBookDTO addressBookDTO) {
+        this.firstName = addressBookDTO.firstName;
+        this.lastName = addressBookDTO.lastName;
+        this.phoneNumber = addressBookDTO.phoneNumber;
+        this.email = addressBookDTO.email;
+        this.address = addressBookDTO.address;
+        this.city = addressBookDTO.city;
+        this.state = addressBookDTO.state;
+        this.zip = addressBookDTO.zip;
+    }
+
+    public void updateData(AddressBookDTO addressBookDTO) {
+        this.firstName = addressBookDTO.firstName;
+        this.lastName = addressBookDTO.lastName;
+        this.phoneNumber = addressBookDTO.phoneNumber;
+        this.email = addressBookDTO.email;
+        this.address = addressBookDTO.address;
+        this.city = addressBookDTO.city;
+        this.state = addressBookDTO.state;
+        this.zip = addressBookDTO.zip;
+    }
+}
